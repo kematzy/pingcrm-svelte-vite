15e91e1c56d83ea9d12122b72270aa21a2464698
Kematzy <kematzy@gmail.com>
2021-07-23 16:57:42 +0800

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,11 @@ import { InertiaProgress } from '@inertiajs/progress'
 InertiaProgress.init()
 
 createInertiaApp({
-  resolve: name => import(`./Pages/${name}.svelte`),
+  // resolve: name => import(`./Pages/${name}.svelte`),
+  resolve: name => {
+    const pages = import.meta.glob('./Pages/**/*.svelte')
+    return pages[`./Pages/${name}.svelte`]().then(module => module.default)
+  },
   setup({ el, App, props }) {
     new App({ target: el, props })
   },
