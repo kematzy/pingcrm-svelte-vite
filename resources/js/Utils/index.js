281b97563a4ca71de10e7ff33d2e9bcb7c9ea6cf
Kematzy <kematzy@gmail.com>
2021-07-23 17:52:59 +0800

    Add missing Utils

--- resources/js/Utils/index.js
+++ resources/js/Utils/index.js
@@ -1,1 +1,9 @@
 export const route = window.route
+
+export function isEnterKey(e) {
+  return e.keyCode && e.keyCode === 13
+}
+
+export function isEscKey(e) {
+  return e.keyCode && e.keyCode === 27
+}
