ee21f2a9df8fafba87aae8e43ae6457166fccba3
Kematzy <kematzy@gmail.com>
2021-07-23 17:51:48 +0800

    Create custom vite() helper &  @viteserver directive

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Http\Client\ConnectionException;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +28,17 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Blade::if('viteserver', function () {
+            if (app()->environment('production')) {
+                return false;
+            }
+
+            try {
+                Http::get('http://localhost:3000');  // TODO: convert to customisable ENV variable
+                return true;
+            } catch (ConnectionException $exception) {
+                return false;
+            }
+        });
     }
 }
