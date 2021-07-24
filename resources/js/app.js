f424c35ee03597522e8d38744f67efc16a7570b8
Kematzy <kematzy@gmail.com>
2021-07-24 19:46:02 +0800

    Improved app.js format
    This kind of works in both Dev and Build (Production) modes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,26 @@
-import Layout from '@/Shared/Layout.svelte'
+// import Layout from '@/Shared/Layout.svelte'  // not sure if this needs to be loaded
 import { createInertiaApp } from '@inertiajs/inertia-svelte'
 import { InertiaProgress } from '@inertiajs/progress'
 
 InertiaProgress.init()
 
+function withVite(pages, name) {
+  for (const path in pages) {
+    if (path.endsWith(`${name.replace('.', '/')}.svelte`)) {
+      return typeof pages[path] === 'function' ? pages[path]() : pages[path]
+    }
+  }
+  throw new Error('Page not found: ' + name)
+}
+
 createInertiaApp({
   // resolve: name => import(`./Pages/${name}.svelte`),
-  resolve: name => {
-    const pages = import.meta.glob('./Pages/**/*.svelte')
-    return pages[`./Pages/${name}.svelte`]().then(module => module.default)
-  },
+  // resolve: (name) => {                                                       // [2]
+  //   const pages = import.meta.glob('./Pages/**/*.svelte')
+  //   return pages[`./Pages/${name}.svelte`]().then((module) => module.default)
+  // },
+  resolve: (name) => withVite(import.meta.glob('./Pages/**/*.svelte'), name),   // [3]
   setup({ el, App, props }) {
     new App({ target: el, props })
-  },
+  }
 })
