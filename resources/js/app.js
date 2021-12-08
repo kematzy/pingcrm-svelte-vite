a1607baced752af8c4696cdbc46a5e7a8d0a64e2
Kematzy <kematzy@gmail.com>
2021-12-08 09:04:44 +0800

    Remove previous debug comments

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,3 @@
-// import 'vite/dynamic-import-polyfill'  // this is not really needed
-
-// import Layout from '@/Shared/Layout.svelte'  // not sure if this needs to be loaded
-// import { Inertia } from '@inertiajs/inertia' // not sure if this needs to be loaded
 import { createInertiaApp } from '@inertiajs/inertia-svelte'
 import { InertiaProgress } from '@inertiajs/progress'
 
@@ -18,35 +14,12 @@ function withVite(pages, name) {
 
 createInertiaApp({
   id: 'app',
-  // resolve: (name) => import(`./Pages/${name}.svelte`),                       // [1]
-
-  // resolve: (name) => {                                                       // [2]
-  //   const pages = import.meta.glob('./Pages/**/*.svelte')
-  //   return pages[`./Pages/${name}.svelte`]().then((module) => module.default)
-  // },
-
-  resolve: (name) => withVite(import.meta.glob('./Pages/**/*.svelte'), name),   // [3]
+  resolve: (name) => withVite(import.meta.glob('./Pages/**/*.svelte'), name),
+  //
   setup({ el, App, props }) {
-    //   console.log('props.initialPage: ', props.initialPage) // contains .initialPage object with all $page values
-    //   console.log('props.initialComponent: ', props.initialComponent) // contains .initialPage object with all $page values
     new App({ target: el, props })
-    // el.removeAttribute('data-page')  // remove unsightly HTML data output
+    if (import.meta.env.PROD) {
+      el.removeAttribute('data-page')  // remove unsightly HTML data output in production
+    }
   },
-  // page: JSON.parse(document.getElementById('app').dataset.page)  // doesn't add anything
 })
-
-// DEBUGGING REFERENCE NOTES:
-//
-// [1]: This version works in Dev mode, BUT does not work in build mode where it fails create pages & components
-//
-//
-// [2]: This version does not work in either Dev or Build modes. In Dev mode the components are loaded, but no content
-//      output is shown in the browser window other than <!--<Render>--> & <!--<App>--> HTML comments in the source.
-//      In Build mode, all the pages & components are generated, but the pages & components are not loading correctly
-//      or at all. No output is appended to div #app, not even some comments.
-//
-//
-// [3]: This version works perfectly in Build mode, without any browser console output, and shows the `$page` variable.
-//      In Dev mode, the Login page works with live updates, but without `$page` variable output.
-//      Due to the `$page` variable being unavailable, all subsequent pages does not work and fails with a
-//      `undefined $page.props...` error.
